5df5d7649fbcc0e1054657e887d63794036d1993
EvanCobb <evan.cobb@protonmail.com>
2020-08-17 18:14:06 -0500

    refactoring to make the next changes easier

--- Broker_DungeonDifficulty.lua
+++ Broker_DungeonDifficulty.lua
@@ -1,86 +1,76 @@
 local BrokerDungeonDifficulty = {}
 
-local ADDON, namespace = ...
-
 local dungeonDiffMap = {}
 dungeonDiffMap[1] = _G.PLAYER_DIFFICULTY1
-dungeonDiffMap[2] = _G.PLAYER_DIFFICULTY2 
+dungeonDiffMap[2] = _G.PLAYER_DIFFICULTY2
 dungeonDiffMap[23] = _G.PLAYER_DIFFICULTY6
-
-local function get_dungeon_diff()
-    local dungeon_id = GetDungeonDifficultyID()
-    return dungeonDiffMap[dungeon_id]
-end
 
 local raidDiffMap = {}
 raidDiffMap[14] = _G.PLAYER_DIFFICULTY1
 raidDiffMap[15] = _G.PLAYER_DIFFICULTY2
 raidDiffMap[16] = _G.PLAYER_DIFFICULTY6
 
-local function get_raid_diff()
-    local raid_id = GetRaidDifficultyID()
-    local out = raidDiffMap[raid_id]
-    assert(out, "Could not find Raid difficulty ID " .. raid_id .. " in to-string map. Have these values changed on Blizzard's side?")
-    return out
-end
-
 local legacyRaidDiffMap = {}
 legacyRaidDiffMap[3] = _G.RAID_DIFFICULTY_10PLAYER
 legacyRaidDiffMap[4] = _G.RAID_DIFFICULTY_25PLAYER
-legacyRaidDiffMap[5] = _G.RAID_DIFFICULTY_10PLAYER_HEROIC 
-legacyRaidDiffMap[6] = _G.RAID_DIFFICULTY_25PLAYER_HEROIC 
+legacyRaidDiffMap[5] = _G.RAID_DIFFICULTY_10PLAYER_HEROIC
+legacyRaidDiffMap[6] = _G.RAID_DIFFICULTY_25PLAYER_HEROIC
 
-local function get_legacy_diff()
-    local legacy_raid_id = GetLegacyRaidDifficultyID()
-    local out = legacyRaidDiffMap[legacy_raid_id]
-    assert(out, "Could not find Legacy Raid difficulty ID " .. legacy_raid_id .. " in to-string map. Have these values changed on Blizzard's side?")
+local function difficulty(description, getter_func, diff_map)
+    local id = getter_func()
+    local out = diff_map[id]
+    assert(out, "Could not find " .. description .. " difficulty ID " .. id ..
+        " in to-string map. Have these values changed on Blizzard's side?")
     return out
+end
+
+local function build_diff_setter(self, difficulty_map, setter_function)
+    for k, v in pairs(difficulty_map) do
+        local line = self:AddLine("  " .. v)
+        local callback = function()
+            setter_function(k)
+        end
+        self:SetLineScript(line, "OnMouseUp", callback)
+    end
 end
 
 function BrokerDungeonDifficulty.build_tooltip(self)
     self:AddHeader(_G.DUNGEON_DIFFICULTY)
     self:AddSeparator()
-    for k, v in pairs(dungeonDiffMap) do
-        local line = self:AddLine("  " .. v)
-        self:SetLineScript(line, "OnMouseUp", function() SetDungeonDifficultyID(k) end)
-    end
-    
+    build_diff_setter(self, dungeonDiffMap, SetDungeonDifficultyID)
+
     self:AddLine("")
 
     self:AddHeader(_G.RAID_DIFFICULTY)
     self:AddSeparator()
-    for k, v in pairs(raidDiffMap) do
-        print(k)
-        print(v)
-        print("")
-        local line = self:AddLine("  " .. v)
-        self:SetLineScript(line, "OnMouseUp", function() SetRaidDifficultyID(k) end)
-    end
-    
+    build_diff_setter(self, raidDiffMap, SetRaidDifficultyID)
+
     self:AddLine("")
 
     self:AddHeader(_G.LEGACY_RAID_DIFFICULTY)
     self:AddSeparator()
-    for k, v in pairs(legacyRaidDiffMap) do
-        local line = self:AddLine("  " .. v)
-        self:SetLineScript(line, "OnMouseUp", function() SetLegacyRaidDifficultyID(k) end)
-    end
+    build_diff_setter(self, legacyRaidDiffMap, SetLegacyRaidDifficultyID)
 end
 
 -- TODO if raid is Mythic, grey out legacy settings
 -- TODO if inside an instance, only show the relevant settings
 -- TODO toggleable shorthand mode w/ cvars on click
 -- TODO fix sorting
-function BrokerDungeonDifficulty.build_label()    
-    return "" .. get_dungeon_diff() .. "/" .. get_raid_diff() .. "/" .. get_legacy_diff()
+-- TODO hook relevant events so we don't have to update every 5s
+function BrokerDungeonDifficulty.build_label()
+    local display = {difficulty("Dungeon", GetDungeonDifficultyID, dungeonDiffMap),
+                     difficulty("Raid", GetRaidDifficultyID, raidDiffMap),
+                     difficulty("Legacy Raid", GetLegacyRaidDifficultyID, legacyRaidDiffMap)}
+    return table.concat(display, " / ")
 end
 
+-------------------
+-- Wiring/LDB/QTip
+-------------------
 
 local ADDON, namespace = ...
 local LibQTip = LibStub('LibQTip-1.0')
-
 local ldb = LibStub:GetLibrary("LibDataBroker-1.1")
-
 local dataobj = ldb:NewDataObject(ADDON, {
     type = "data source",
     text = "-"
@@ -105,20 +95,17 @@ local function anchor_OnEnter(self)
 
     BrokerDungeonDifficulty.build_tooltip(tooltip)
 
-    -- Use smart anchoring code to anchor the tooltip to our frame
     tooltip:SmartAnchorTo(self)
 
-    -- Show it, et voilà !
     tooltip:Show()
 end
 
--- tooltip/broker object settings
 function dataobj:OnEnter()
     anchor_OnEnter(self)
 end
 
+--- Nothing to do. Needs to be defined for some display addons apparently
 function dataobj:OnLeave()
-    -- Nothing to do. Needs to be defined for some display addons apparently
 end
 
 function dataobj:OnClick()
