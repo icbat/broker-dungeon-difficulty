87de4799c61c416d78699737409bd4c54aa241ca
EvanCobb <evan.cobb@protonmail.com>
2020-08-17 23:17:19 -0500

    faster feedback, tooltip shows what you have selected

--- Broker_DungeonDifficulty.lua
+++ Broker_DungeonDifficulty.lua
@@ -1,9 +1,6 @@
--- TODO if raid is Mythic, grey out legacy settings
+-- TODO if raid is Mythic, grey out legacy display?
 -- TODO if inside an instance, only show the relevant settings
--- TODO toggleable shorthand mode w/ cvars on click
--- TODO hook relevant events so we don't have to update every 5s
--- TODO tooltip should show a colored asterisk if that's what you've already selected
--- TODO tooltip line should not be clickable if that's what you've already selected
+-- TODO toggleable shorthand mode w/ savedvariables on click
 
 -- the map tables are auto-sorting based on the strings, this is to preserve manual ordering
 local function array_to_map(array)
@@ -15,23 +12,25 @@ local function array_to_map(array)
 end
 
 local dungeon_display = {
-    {id = 1, display = _G.PLAYER_DIFFICULTY1},
-    {id = 2, display = _G.PLAYER_DIFFICULTY2},
-    {id = 23, display = _G.PLAYER_DIFFICULTY6},
+    { id = 1, display = _G.PLAYER_DIFFICULTY1 },
+    { id = 2, display = _G.PLAYER_DIFFICULTY2 },
+    { id = 23, display = _G.PLAYER_DIFFICULTY6 },
 }
 
 local raid_display = {
-    {id = 14, display = _G.PLAYER_DIFFICULTY1},
-    {id = 15, display = _G.PLAYER_DIFFICULTY2},
-    {id = 16, display = _G.PLAYER_DIFFICULTY6},
+    { id = 14, display = _G.PLAYER_DIFFICULTY1 },
+    { id = 15, display = _G.PLAYER_DIFFICULTY2 },
+    { id = 16, display = _G.PLAYER_DIFFICULTY6 },
 }
 
 local legacy_display = {
-    { id = 3, display = _G.RAID_DIFFICULTY_10PLAYER},
-    { id = 5, display = _G.RAID_DIFFICULTY_10PLAYER_HEROIC},
-    { id = 4, display = _G.RAID_DIFFICULTY_25PLAYER},
-    { id = 6, display = _G.RAID_DIFFICULTY_25PLAYER_HEROIC},
+    { id = 3, display = _G.RAID_DIFFICULTY_10PLAYER },
+    { id = 5, display = _G.RAID_DIFFICULTY_10PLAYER_HEROIC },
+    { id = 4, display = _G.RAID_DIFFICULTY_25PLAYER },
+    { id = 6, display = _G.RAID_DIFFICULTY_25PLAYER_HEROIC },
 }
+
+local green = "0000ff00"
 
 local dungeonDiffMap = array_to_map(dungeon_display)
 local raidDiffMap = array_to_map(raid_display)
@@ -45,32 +44,46 @@ local function difficulty(description, getter_func, diff_map)
     return out
 end
 
-local function build_diff_setter(self, difficulty_map, setter_function)
+local function build_diff_setter(self, difficulty_map, getter_function, setter_function)
     for _, el in ipairs(difficulty_map) do
-        local line = self:AddLine("  " .. el.display)
-        local callback = function()
-            setter_function(el.id)
+        local selected_id = getter_function()
+        local prefix = ""
+
+        if selected_id == el.id then
+            prefix = "\124c" .. green .. ">" .. "\124r"
         end
-        self:SetLineScript(line, "OnMouseUp", callback)
+
+        local line = self:AddLine(prefix, el.display)
+
+        if selected_id ~= el.id then
+            local callback = function()
+                setter_function(el.id)
+                self:Clear()
+                build_tooltip(self)
+            end
+            self:SetLineScript(line, "OnMouseUp", callback)
+        end
     end
 end
 
 local function build_tooltip(self)
-    self:AddHeader(_G.DUNGEON_DIFFICULTY)
+    -- col 1 is for highlighting what you're currently queued for
+    -- col 2 is general text
+    self:AddHeader("", _G.DUNGEON_DIFFICULTY)
     self:AddSeparator()
-    build_diff_setter(self, dungeon_display, SetDungeonDifficultyID)
+    build_diff_setter(self, dungeon_display, GetDungeonDifficultyID, SetDungeonDifficultyID)
 
     self:AddLine("")
 
-    self:AddHeader(_G.RAID_DIFFICULTY)
+    self:AddHeader("", _G.RAID_DIFFICULTY)
     self:AddSeparator()
-    build_diff_setter(self, raid_display, SetRaidDifficultyID)
+    build_diff_setter(self, raid_display, GetRaidDifficultyID, SetRaidDifficultyID)
 
     self:AddLine("")
 
-    self:AddHeader(_G.LEGACY_RAID_DIFFICULTY)
+    self:AddHeader("", _G.LEGACY_RAID_DIFFICULTY)
     self:AddSeparator()
-    build_diff_setter(self, legacy_display, SetLegacyRaidDifficultyID)
+    build_diff_setter(self, legacy_display, GetLegacyRaidDifficultyID, SetLegacyRaidDifficultyID)
 end
 
 local function build_label()
@@ -103,7 +116,7 @@ local function anchor_OnEnter(self)
         self.tooltip = nil
     end
 
-    local tooltip = LibQTip:Acquire(ADDON, 1, "LEFT")
+    local tooltip = LibQTip:Acquire(ADDON, 2, "LEFT", "LEFT")
     self.tooltip = tooltip
     tooltip.OnRelease = OnRelease
     tooltip.OnLeave = OnLeave
@@ -128,13 +141,13 @@ function dataobj:OnClick()
     ToggleLFDParentFrame()
 end
 
-function set_label(self)
+local function set_label(self)
     dataobj.text = build_label()
 end
 
 -- invisible frame for updating/hooking events
 local f = CreateFrame("frame")
-local UPDATEPERIOD = 5
+local UPDATEPERIOD = 0.5
 local elapsed = 0
 f:SetScript("OnUpdate", function(self, elap)
     elapsed = elapsed + elap
@@ -145,5 +158,10 @@ f:SetScript("OnUpdate", function(self, elap)
     set_label(self)
 end)
 
+-- https://www.townlong-yak.com/framexml/live/Blizzard_APIDocumentation#PLAYER_DIFFICULTY_CHANGED
+-- "This is claimed to exist." 
+f:RegisterEvent("PLAYER_DIFFICULTY_CHANGED")
+-- on login
 f:RegisterEvent("PLAYER_ENTERING_WORLD")
 f:SetScript("OnEvent", set_label)
+
